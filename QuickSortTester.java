91584ba0479a9a0ef1e7353b7d0a93502c543be3
LarWong <lwong7@stuy.edu>
2018-03-15 13:53:34 -0400

    added set pivot

--- QuickSortTester.java
+++ QuickSortTester.java
@@ -5,7 +5,8 @@ public class QuickSortTester {
 	//warming up
 	int n = 1000;
 	int[] arr;
-	System.out.println("Random Pivot:\nSize (n) , Time(Nanoseconds)");
+	/*
+	System.out.println("\nRandom Pivot:\nSize (n) , Time(Nanoseconds)");
 
 	//measure time for each array generated, takes the average
 	while (n <= 100000){
@@ -39,16 +40,17 @@ public class QuickSortTester {
 	    n += 1000;
 
 	}
+	*/
 	
-	System.out.println("Set Pivot:\nSize (n) , Time(Nanoseconds)");
+	System.out.println("\nSet Pivot(right most):\nSize (n) , Time(Nanoseconds)");
 	//measure time for each array generated, takes the average
-	while (n <= 100000){
+	while (n <= 10000){
 	    //populating new awway
 	    arr = new int[n];
 	    //This process runs in O(n) for each array generated. It is NOT counted for time.
-	    //This process will populate the array with random integers.
+	    //This process will populate the array with same integers (counts as sorted)
 	    for( int i = 0; i < n; i++ )
-		arr[i] = (int)( 48 * Math.random() );
+		arr[i] = 1;
 	
 	    //Total time
 	    double total = 0.0;
@@ -57,9 +59,8 @@ public class QuickSortTester {
 
 	    //We test arrays of the same length 100 times and take the average to yield accurate results, remove outliers, and in the name of proper data gathering
 	    while (counter < 100) {
-		code.shuffle(arr);
 		long startTime = System.nanoTime();
-		code.qsort(arr);
+		code.qsortSet(arr);
 		long endTime = System.nanoTime() - startTime;
 		total += endTime;
 
@@ -70,7 +71,7 @@ public class QuickSortTester {
 	    total = total / 100.0;
 	    System.out.println(n + " , " + total);
 	    //Increase the size of the array by 1000
-	    n += 1000;
+	    n += 100;
 
 	}
 
